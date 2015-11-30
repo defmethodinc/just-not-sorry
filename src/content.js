0f6950b24fbc8bd34700ff7e372f5e6cc3d7ffe3
Steve Brudz <sbrudz@gmail.com>
2015-11-30 13:58:17 -0500

    Moved .js files to src and lib directories to prepare for jasmine setup

--- src/content.js
+++ src/content.js
@@ -1,15 +1,15 @@
 var j = document.createElement('script');
-j.src = chrome.extension.getURL('jquery-2.1.4.min.js');
+j.src = chrome.extension.getURL('lib/jquery-2.1.4.min.js');
 (document.head || document.documentElement).appendChild(j);
 
 var k = document.createElement('script');
-k.src = chrome.extension.getURL('findAndReplaceDOMText.js');
+k.src = chrome.extension.getURL('lib/findAndReplaceDOMText.js');
 (document.head || document.documentElement).appendChild(k);
 
 var g = document.createElement('script');
-g.src = chrome.extension.getURL('gmail-0.4.js');
+g.src = chrome.extension.getURL('lib/gmail-0.4.js');
 (document.head || document.documentElement).appendChild(g);
 
 var s = document.createElement('script');
-s.src = chrome.extension.getURL('just-not-sorry.js');
+s.src = chrome.extension.getURL('src/just-not-sorry.js');
 (document.head || document.documentElement).appendChild(s);
