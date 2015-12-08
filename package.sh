5711e6ab19d78c17b7071ad73c8c9f6d2655da28
Steve Brudz <sbrudz@gmail.com>
2015-12-08 16:02:21 -0500

    Change package.sh to append version # from manifest.json

--- package.sh
+++ package.sh
@@ -1,3 +1,4 @@
 #!/bin/sh
-zip -r just-not-sorry.zip . -i@include.lst -x lib/jasmine-2.3.4/*.js \
+VERSION=`grep '"version":' manifest.json | cut -d: -f 2 | tr -d "\"\,\ "`
+zip -r "just-not-sorry-$VERSION.zip" . -i@include.lst -x lib/jasmine-2.3.4/*.js \
 lib/jasmine-jquery.js
