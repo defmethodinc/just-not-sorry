e4d8caaefb9a972e14b8da89c0aa12795c12a7df
Steve Brudz <sbrudz@gmail.com>
2015-12-10 12:31:40 -0500

    Add source information for messages to warnings file

--- src/Warnings.js
+++ src/Warnings.js
@@ -1,26 +1,33 @@
 var WARNINGS = {
   warnings: [
     { keyword: 'just',
+      source:  'http://www.taramohr.com/8-ways-women-undermine-themselves-with-their-words/',
       message: '"Just" demeans what you have to say. “Just” shrinks your power. ' +
                'It’s time to say goodbye to the justs. --Tara Sophia Mohr', },
     { keyword: 'actually',
+      source:  'http://www.taramohr.com/8-ways-women-undermine-themselves-with-their-words/',
       message: '"Actually" communicates a sense of surprise that you have ' +
                'something to say. Of course you want to add something. Of ' +
                'course you have questions. There’s nothing surprising about ' +
                'it.  --Tara Sophia Mohr', },
     { keyword: 'sorry',
+      source:  'http://www.fastcompany.com/3032112/strong-female-lead/sorry-not-sorry-why-women-need-to-stop-apologizing-for-everything',
       message: 'Using "sorry" frequently undermines your gravitas and makes you ' +
                'appear unfit for leadership. --Sylvia Ann Hewlett', },
     { keyword: 'apologize',
+      source:  'http://www.fastcompany.com/3032112/strong-female-lead/sorry-not-sorry-why-women-need-to-stop-apologizing-for-everything',
       message: 'Apologizing unnecessarily puts you in a subservient position and ' +
                'makes people lose respect for you --Bonnie Marcus', },
     { keyword: 'I think',
+      source:  'http://www.fastcompany.com/3049609/the-future-of-work/4-types-of-useless-phrases-you-need-to-eliminate-from-your-emails',
       message: '"I think" undermines your idea and displays an overall lack of ' +
                'self-confidence. --Lydia Dishman', },
     { keyword: 'I\'m no expert',
+      source:  'http://www.fastcompany.com/3049609/the-future-of-work/4-types-of-useless-phrases-you-need-to-eliminate-from-your-emails',
       message: '"I\'m no expert" undermines your idea and displays an overall ' +
               'lack of self-confidence. --Lydia Dishman', },
     { keyword: 'does that make sense',
+      source:  'http://goop.com/how-women-undermine-themselves-with-words/',
       message: '"does that make sense" comes across either as condescending ' +
                '(like your audience can\'t understand) or it implies you feel ' +
                'you’ve been incoherent. A better way to close is something like ' +
