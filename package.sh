e9b8ad0796df8ece6377628c3ef3cf46d8ab58be
Steve Brudz <sbrudz@gmail.com>
2015-12-10 10:37:04 -0500

    Move test libs into directory under spec

--- package.sh
+++ package.sh
@@ -1,4 +1,3 @@
 #!/bin/sh
 VERSION=`grep '"version":' manifest.json | cut -d: -f 2 | tr -d "\"\,\ "`
-zip -r "just-not-sorry-$VERSION.zip" . -i@include.lst -x lib/jasmine-2.3.4/*.js \
-lib/jasmine-jquery.js
+zip -r "just-not-sorry-$VERSION.zip" . -i@include.lst
