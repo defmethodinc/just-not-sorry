3434623f7b2ffa4cb48ca0c1f95a140d40d8ec2f
Steve Brudz <sbrudz@gmail.com>
2015-12-18 15:32:44 -0500

    Fix 'Uncaught TypeError' when running with SalesforceIQ extension.
    by upgrading to patched version of gmail.js
    
    https://github.com/sbrudz/gmail.js/commit/4fb62632eca1f9c89cd4b2131e2ff1c2950207dd

--- src/ScriptLoader.js
+++ src/ScriptLoader.js
@@ -2,7 +2,7 @@ var scriptsToLoad = [
   chrome.extension.getURL('lib/jquery-2.1.4.min.js'),
   chrome.extension.getURL('lib/jquery.caret.min.js'),
   chrome.extension.getURL('lib/findAndReplaceDOMText.js'),
-  chrome.extension.getURL('lib/gmail-0.4.js'),
+  chrome.extension.getURL('lib/gmail.js'),
   chrome.extension.getURL('src/WarningChecker.js'),
   chrome.extension.getURL('src/Warnings.js'),
   chrome.extension.getURL('src/JustNotSorry.js'),
