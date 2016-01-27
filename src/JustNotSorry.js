c36981f5cf4b275e68ab0deee6d810609bce11fa
Steve Brudz <sbrudz@gmail.com>
2016-01-26 23:13:53 -0500

    Spike to try out using an overlay to display highlighting for words.
    Currently only "just" is highlighted
    
    Goal is to fix issues #10, #12, #25, #27 by using a different approach

--- src/JustNotSorry.js
+++ src/JustNotSorry.js
@@ -20,7 +20,7 @@ var JustNotSorry = function() {
     $target.focusin(function(e) {
       var body = compose.dom('body');
       if (e.target === body.get(0)) {
-        warningChecker.addWarnings(body);
+        // warningChecker.addWarnings(body);
       }
     });
   }
@@ -30,21 +30,61 @@ var JustNotSorry = function() {
     $target.focusout(function(e) {
       var body = compose.dom('body');
       if (e.target === body.get(0)) {
-        warningChecker.removeWarnings(body);
+        // warningChecker.removeWarnings(body);
       }
     });
   }
+
+  // div highlight needs to scroll with email text
+  // div highlight should pass-through mouse clicks
+  // hover over div highlight should display tooltip
+  // Expand MutationObserver to handle increased font sizes
+
+  // all the same words should be supported
+  // add and remove in the same way
+  // remove caretPosition code
 
   function updateWarningsOnMutation(compose) {
     var target = compose.$el.get(0);
     var observer = new MutationObserver(function() {
       var body = compose.dom('body');
-      var caretPosition = body.caret('pos');
-      warningChecker.removeWarnings(body);
-      warningChecker.addWarnings(body);
-      body.caret('pos', caretPosition);
+      if (body.text().includes('just')) {
+        var range = document.createRange();
+        var index = body.text().indexOf('just');
+        // debugger;
+        var bodyNode = body.get(0);
+        var child = bodyNode.firstChild;
+        range.setStart(child, index);
+        range.setEnd(child, index+4);
+        drawBox(range.getBoundingClientRect(), bodyNode.parentNode);
+      }
+      // var caretPosition = body.caret('pos');
+      // warningChecker.removeWarnings(body);
+      // warningChecker.addWarnings(body);
+      // body.caret('pos', caretPosition);
     });
     observer.observe(target, {characterData: true, subtree: true});
+  }
+
+  function drawBox(rect, parentNode) {
+    var parentRect = parentNode.getBoundingClientRect();
+
+    var tableRectDiv = document.createElement('div');
+    tableRectDiv.id = 'steve-test';
+    tableRectDiv.className = 'jns-warning';
+    tableRectDiv.title = 'This is a test';
+    tableRectDiv.style.position = 'absolute';
+    // tableRectDiv.style.pointerEvents = 'none';
+
+    var scrollTop = document.documentElement.scrollTop || document.body.scrollTop;
+    var scrollLeft = document.documentElement.scrollLeft || document.body.scrollLeft;
+    tableRectDiv.style.margin = tableRectDiv.style.padding = '0';
+    tableRectDiv.style.top = (rect.top + scrollTop - parentRect.top + rect.height*0.9) + 'px';
+    tableRectDiv.style.left = (rect.left + scrollLeft - parentRect.left) + 'px';
+    tableRectDiv.style.width = (rect.width) + 'px';
+    tableRectDiv.style.height = (rect.height*0.25) + 'px';
+    tableRectDiv.style.zIndex = 10;
+    parentNode.appendChild(tableRectDiv);
   }
 
   function checkForWarnings(compose) {
