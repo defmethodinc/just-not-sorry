6624727d620c24fa3561681eee970579a9fd4724
Mohamed ElSayed <magnifismail@gmail.com>
2016-05-25 20:00:45 +0200

    improve ui, fix conflict with SalesforceIQ 1. Improve UI, and suggestions to use Polymer's web components. 2. Delay inialization to fix the conflict with SalesforceIQ. 3. Upgrade jQuery to 2.2.4. 4. Remove head.js

--- src/WarningChecker.js
+++ src/WarningChecker.js
@@ -4,22 +4,38 @@ function WarningChecker(options) {
   this.warningClass = options.warningClass || 'jns-warning';
 }
 
-WarningChecker.prototype.addWarning = function addWarning($content, keyword, message) {
+WarningChecker.prototype.addWarning = function addWarning($content, keyword, message, warn) {
   'use strict';
   var pattern = new RegExp('\\b(' + keyword + ')(?!($))\\b', 'ig');
-  domRegexpMatch($content.get(0), pattern, HighlightGenerator.highlightMatches(message, this.warningClass));
+  domRegexpMatch($content.get(0), pattern, HighlightGenerator.highlightMatches(message, this.warningClass, warn));
+};
+
+WarningChecker.prototype.getCursorPosition = function addWarnings($content) {
+  var sel = document.getSelection();
 };
 
 WarningChecker.prototype.addWarnings = function addWarnings($content) {
   'use strict';
   var _this = this;
+  this.hasWarnings = true;
+  window.$content = $content;
+  _this.getCursorPosition($content);
   $.each(_this.warnings, function(index, warning) {
-    _this.addWarning($content, warning.keyword, warning.message);
+    _this.addWarning($content, warning.keyword, warning.message, warning);
   });
+
 };
 
 WarningChecker.prototype.removeWarnings = function removeWarnings($content) {
   'use strict';
-  var $elementsToRemove = $content.find('div.' + this.warningClass);
-  $elementsToRemove.remove();
+  if (!this.hasWarnings) return;
+  var elementsToRemove = $content.find('e.chrome-jns-highlight');
+  elementsToRemove.each(function() {
+    var elm = this,
+      parent = elm.parentElement,
+      textNode = elm.firstChild;
+    parent.replaceChild(textNode, elm);
+
+  });
+  window.chromePopup.toggle(false);
 };
