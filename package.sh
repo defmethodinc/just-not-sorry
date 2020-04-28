f1ee7137b176dab42a66546670e13b0602dfd213
Steve Brudz <sbrudz@defmethod.io>
2020-04-28 15:02:08 -0500

    build: enhance package.sh to also update the version in package.json
    this enables semantic release to commit both of those files with updated version numbers whenever a
    release occurs

--- package.sh
+++ package.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 VERSION=$1
-sed -i.old "s/\"version\": \".*\"/\"version\": \"$VERSION\"/" manifest.json
+echo "manifest.json package.json" | xargs sed -i.old "s/\"version\": \".*\"/\"version\": \"$VERSION\"/"
 mkdir -p dist
 zip -r "dist/just-not-sorry-chrome.zip" . -i@include.lst
