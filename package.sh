982d27a57ecca42ced549b05bd60ad1eb7e0879e
Steve Brudz <sbrudz@defmethod.io>
2020-04-28 10:45:26 -0500

    build: fix package.sh script so that it runs on ubuntu (for the CI build)

--- package.sh
+++ package.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 VERSION=$1
-sed -i .old "s/\"version\": \".*\"/\"version\": \"$VERSION\"/" manifest.json
+sed -i.old "s/\"version\": \".*\"/\"version\": \"$VERSION\"/" manifest.json
 mkdir -p dist
 zip -r "dist/just-not-sorry-chrome.zip" . -i@include.lst
