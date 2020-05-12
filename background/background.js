5ee81babd6e36e0ba3773d9007c50d609b47c9d0
Steve Brudz <sbrudz@defmethod.io>
2020-05-12 16:06:08 -0500

    style: add prettier and apply formatting to code

--- background/background.js
+++ background/background.js
@@ -1,12 +1,12 @@
 var gaService = analytics.getService('JustNotSorryTest');
 
-chrome.runtime.onInstalled.addListener(function() {
+chrome.runtime.onInstalled.addListener(function () {
   var tracker = gaService.getTracker('UA-3535278-4'); // prod
   var timing = tracker.startTiming('Analytics Performance', 'Send Event');
   tracker.sendAppView('JustNotSorryInstalled');
   timing.send();
 
-  chrome.declarativeContent.onPageChanged.removeRules(undefined, function() {
+  chrome.declarativeContent.onPageChanged.removeRules(undefined, function () {
     chrome.declarativeContent.onPageChanged.addRules([
       {
         conditions: [
@@ -15,18 +15,19 @@ chrome.runtime.onInstalled.addListener(function() {
           }),
           new chrome.declarativeContent.PageStateMatcher({
             pageUrl: { urlContains: 'inbox.google.com' },
-          })
+          }),
         ],
-        actions: [ new chrome.declarativeContent.ShowPageAction() ]
-      }
+        actions: [new chrome.declarativeContent.ShowPageAction()],
+      },
     ]);
   });
 });
 
-chrome.pageAction.onClicked.addListener(function() {
+chrome.pageAction.onClicked.addListener(function () {
   if (chrome.runtime.openOptionsPage) {
     chrome.runtime.openOptionsPage();
-  } else { // fallback to old way
+  } else {
+    // fallback to old way
     window.open(chrome.runtime.getURL('options.html'));
   }
 });
