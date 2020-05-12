d21e81384329940159e41e4c383319ae905d2adc
Christine Xiang <christine@defmethod.io>
2020-05-12 16:18:31 -0400

    fix(package.sh): update script to run build as part of release process
    n

--- package.sh
+++ package.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 VERSION=$1
 echo "manifest.json package.json" | xargs sed -i.old "s/\"version\": \".*\"/\"version\": \"$VERSION\"/"
+npm run build
 mkdir -p dist
 cd build
 zip -r "../dist/just-not-sorry-chrome.zip" . *
