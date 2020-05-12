aec3be50df650a6ab6af7e0fcd318aa06f4505ba
Christine Xiang <christine@defmethod.io>
2020-05-12 12:29:40 -0400

    build: update package.sh to use build folder and update README instructions

--- package.sh
+++ package.sh
@@ -2,4 +2,5 @@
 VERSION=$1
 echo "manifest.json package.json" | xargs sed -i.old "s/\"version\": \".*\"/\"version\": \"$VERSION\"/"
 mkdir -p dist
-zip -r "dist/just-not-sorry-chrome.zip" . -i@include.lst
+cd build
+zip -r "../dist/just-not-sorry-chrome.zip" . *
