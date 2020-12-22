abeb48afd962900f27a8ff66c2d2542882a06de9
Steve Brudz <sbrudz@defmethod.io>
2020-12-21 18:42:00 -0600

    build: fix security vulnerability by upgrading copy-webpack-plugin

--- webpack.config.js
+++ webpack.config.js
@@ -3,13 +3,15 @@ const CopyPlugin = require('copy-webpack-plugin');
 
 module.exports = {
   plugins: [
-    new CopyPlugin([
-      { from: 'img', to: 'img' },
-      { from: 'manifest.json' },
-      { from: 'options', to: 'options' },
-      { from: 'background', to: 'background' },
-      { from: 'just-not-sorry.css' },
-    ]),
+    new CopyPlugin({
+      patterns: [
+        { from: 'img', to: 'img' },
+        { from: 'manifest.json' },
+        { from: 'options', to: 'options' },
+        { from: 'background', to: 'background' },
+        { from: 'just-not-sorry.css' },
+      ],
+    }),
   ],
   mode: 'production',
   entry: './src/index.js',
