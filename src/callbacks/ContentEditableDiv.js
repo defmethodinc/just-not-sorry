aa0f1cc1ae9ac4d6f0e3371f007a593eb5815766
James Duffy <james@defmethod.io>
2021-02-17 16:02:52 -0500

    fix: outlook doesn't id its contenteditable divs
    providing a temp id as essentially each message in outlook is unique

--- src/callbacks/ContentEditableDiv.js
+++ src/callbacks/ContentEditableDiv.js
@@ -6,10 +6,11 @@ function isEmailMessageBody(mutation) {
 }
 export const forEachUniqueContentEditable = (action) => {
   const uniqueIds = new Set();
+  let tempId = 0;
   return (mutations) => {
     mutations.filter(isEmailMessageBody).forEach((mutation) => {
       if (!uniqueIds.has(mutation.target.id)) {
-        const id = mutation.target.id;
+        const id = mutation.target.id || tempId++;
         uniqueIds.add(id);
         action(mutation);
       }
