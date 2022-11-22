33feefa08f8601529d26d770c4529f16f4410ea8
Steve Brudz <sbrudz@defmethod.io>
2022-11-22 16:51:06 -0600

    chore: change semantic-release config to be a js file

---
+++ release.config.js
@@ -0,0 +1,44 @@
+const { GIT_BRANCH: branch } = process.env;
+const prepareCmd = './package.sh ${nextRelease.version}';
+const githubAssets = [
+  {
+    path: 'dist/just-not-sorry-chrome.zip',
+    name: 'just-not-sorry-${nextRelease.gitTag}.zip',
+    label: 'Chrome Web Store package',
+  },
+];
+const gitAssetsToUpdate = ['package.json', 'manifest.json'];
+
+module.exports = {
+  dryRun: true, // TODO: remove me
+  branches: [
+    {
+      name: 'main',
+    },
+    {
+      name: 'beta',
+      prerelease: true,
+    },
+    {
+      // TODO: remove me
+      name: 'automate-release-process',
+      prerelease: true,
+    },
+  ],
+  plugins:
+    branch === 'main'
+      ? [
+          ['@semantic-release/commit-analyzer'],
+          ['@semantic-release/release-notes-generator', { linkCompare: false }],
+          ['@semantic-release/exec', { prepareCmd }],
+          ['@semantic-release/github', { assets: githubAssets }],
+          ['@semantic-release/git', { assets: gitAssetsToUpdate }],
+        ]
+      : [
+          ['@semantic-release/commit-analyzer'],
+          ['@semantic-release/release-notes-generator', { linkCompare: false }],
+          ['@semantic-release/exec', { prepareCmd }],
+          ['@semantic-release/github', { assets: githubAssets }],
+          ['@semantic-release/git', { assets: gitAssetsToUpdate }],
+        ],
+};
