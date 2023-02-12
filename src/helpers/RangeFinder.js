00db0afbfd5f57492837fe9377e8b9e2dc3c684e
James Duffy <jduffy3@gmail.com>
2023-02-12 13:10:07 -0500

    refactor: inline private method search

--- src/helpers/RangeFinder.js
+++ src/helpers/RangeFinder.js
@@ -1,12 +1,10 @@
 import * as Util from './util';
 
-function search(element, phrase) {
-  return Util.match(element, phrase.regex).map((range) => ({
-    message: phrase.message,
-    rangeToHighlight: range,
-  }));
-}
-
 export function findRanges(node, patternsToFind) {
-  return patternsToFind.flatMap((pattern) => search(node, pattern));
+  return patternsToFind.flatMap((pattern) =>
+    Util.match(node, pattern.regex).map((range) => ({
+      message: pattern.message,
+      rangeToHighlight: range,
+    }))
+  );
 }
