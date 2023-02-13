5bf0378198eb57c71d9f3df084b19d78047c26d6
James Duffy <jduffy3@gmail.com>
2023-02-12 21:59:35 -0500

    refactor: use util lib to match text on email as it uses text node iterator

--- src/helpers/RangeFinder.js
+++ src/helpers/RangeFinder.js
@@ -1,21 +1,10 @@
 import * as Util from './util';
 
-const textNodeIterator = (node) =>
-  document.createNodeIterator(node, NodeFilter.SHOW_TEXT);
-
 export function findRanges(node, patternsToFind) {
-  const result = [];
-  const iter = textNodeIterator(node);
-  let nextNode;
-  while ((nextNode = iter.nextNode()) !== null) {
-    result.push(
-      ...patternsToFind.flatMap((pattern) =>
-        Util.match(nextNode, pattern.regex).map((range) => ({
-          message: pattern.message,
-          rangeToHighlight: range,
-        }))
-      )
-    );
-  }
-  return result;
+  return patternsToFind.flatMap((pattern) =>
+    Util.match(node, pattern.regex).map((range) => ({
+      message: pattern.message,
+      rangeToHighlight: range,
+    }))
+  );
 }
