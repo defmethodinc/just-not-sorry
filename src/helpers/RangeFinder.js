251ee7dad898b3bfa07ca27cff4221ff345fca87
James Duffy <jduffy3@gmail.com>
2023-02-13 05:45:49 -0500

    Revert "refactor: use util lib to match text on email as it uses text node iterator"
    This reverts commit 5bf0378198eb57c71d9f3df084b19d78047c26d6.

--- src/helpers/RangeFinder.js
+++ src/helpers/RangeFinder.js
@@ -1,10 +1,21 @@
 import * as Util from './util';
 
+const textNodeIterator = (node) =>
+  document.createNodeIterator(node, NodeFilter.SHOW_TEXT);
+
 export function findRanges(node, patternsToFind) {
-  return patternsToFind.flatMap((pattern) =>
-    Util.match(node, pattern.regex).map((range) => ({
-      message: pattern.message,
-      rangeToHighlight: range,
-    }))
-  );
+  const result = [];
+  const iter = textNodeIterator(node);
+  let nextNode;
+  while ((nextNode = iter.nextNode()) !== null) {
+    result.push(
+      ...patternsToFind.flatMap((pattern) =>
+        Util.match(nextNode, pattern.regex).map((range) => ({
+          message: pattern.message,
+          rangeToHighlight: range,
+        }))
+      )
+    );
+  }
+  return result;
 }
