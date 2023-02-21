e37880610ca5394d973e57d89b92ecabe9d750f4
James Duffy <jduffy3@gmail.com>
2023-02-21 10:52:02 -0500

    fix: microsoft Outlook does'nt set element id & intermittently returns null on bounding client rect
    Noticed intermittent error on extensino Errors pane; Didn't observe any breaking behavior.
    Unfortunately with no id, we have to go back to clearing and re-checking all warnings within an
    email because there are no unique ids used across an opened email session when switching to
    different emails.

--- src/callbacks/ContentEditableDiv.js
+++ src/callbacks/ContentEditableDiv.js
@@ -6,10 +6,12 @@ export const forEachUniqueContentEditable = (action) => {
   let tempId = 0;
   return (mutations) => {
     mutations.forEach((mutation) => {
-      if (isEmailMessageBody(mutation) && !uniqueIds.has(mutation.target.id)) {
+      if (isEmailMessageBody(mutation)) {
         const id = mutation.target.id || tempId++;
-        uniqueIds.add(id);
-        action(mutation);
+        if (!uniqueIds.has(id)) {
+          uniqueIds.add(id);
+          action(mutation);
+        }
       }
     });
   };
--- src/components/JustNotSorry.js
+++ src/components/JustNotSorry.js
@@ -1,9 +1,9 @@
 // eslint-disable-next-line no-unused-vars
 import { h } from 'preact';
 import ReactDOM from 'react-dom';
-import {useEffect, useRef, useState} from 'preact/hooks';
+import { useEffect, useRef, useState } from 'preact/hooks';
 import * as Util from '../helpers/util.js';
-import {forEachUniqueContentEditable} from '../callbacks/ContentEditableDiv';
+import { forEachUniqueContentEditable } from '../callbacks/ContentEditableDiv';
 import Warning from './Warning';
 
 const JustNotSorry = ({ phrases, onEvents }) => {
@@ -12,28 +12,34 @@ const JustNotSorry = ({ phrases, onEvents }) => {
   const [observer, setObserver] = useState(null);
   const [warnings, setWarnings] = useState([]);
 
-  const hideWarnings = () => {
-    if (warningContainer.current) {
+  const hideWarnings = ({ target }) => {
+    if (hasId(target) && warningContainer.current) {
       warningContainer.current.style.visibility = 'hidden';
+    } else {
+      //no id to cache, clear everything (Microsoft Outlook)
+      setWarnings([]);
     }
   };
 
-  const displayWarnings = ({target}) => {
+  const hasId = (target) => target.id !== '';
+
+  const displayWarnings = ({ target }) => {
     const current = warningContainer.current;
-    if (current && current.dataset.emailId === target.id) {
+    if (hasId(target) && current && current.dataset.emailId === target.id) {
       current.style.visibility = 'visible';
       return true;
     }
-    return false
-  }
+    return false;
+  };
 
-  const displayOr = (elseAction) => (e) =>  {
-    if(!displayWarnings(e)) {
-      elseAction()
+  const displayOr = (elseAction) => (e) => {
+    if (!displayWarnings(e)) {
+      elseAction();
     }
   };
 
-  const findWarnings = (target) => Util.debounce(() => search(target), Util.WAIT_TIME);
+  const findWarnings = (target) =>
+    Util.debounce(() => search(target), Util.WAIT_TIME);
 
   const applyEventListeners = ({ target }) => {
     const updateWarnings = findWarnings(target);
@@ -66,31 +72,39 @@ const JustNotSorry = ({ phrases, onEvents }) => {
     observer,
   ]);
 
-  const textNodeIterator = (node) => document.createNodeIterator(node, NodeFilter.SHOW_TEXT);
+  const textNodeIterator = (node) =>
+    document.createNodeIterator(node, NodeFilter.SHOW_TEXT);
 
   const search = (email) => {
     const warnings = [];
-    const emailBounds = email.offsetParent.getBoundingClientRect();
+    const emailBounds = email.offsetParent?.getBoundingClientRect();
     let textNode;
     const iter = textNodeIterator(email);
     while ((textNode = iter.nextNode()) !== null) {
       for (let i = 0; i < phrases.length; i++) {
-        const {regex, message} = phrases[i];
+        const { regex, message } = phrases[i];
         const ranges = Util.match(textNode, regex);
         for (let j = 0; j < ranges.length; j++) {
           const range = ranges[j];
-          let key = `${email.offsetTop + range.startOffset}x${email.offsetLeft + range.endOffset}`;
-          if(range.startContainer.parentElement){
-            const {offsetTop, offsetLeft} = range.startContainer.parentElement;
-            key = `${offsetTop + range.startOffset}x${offsetLeft + range.endOffset}`;
+          let key = `${email.offsetTop + range.startOffset}x${
+            email.offsetLeft + range.endOffset
+          }`;
+          if (range.startContainer.parentElement) {
+            const {
+              offsetTop,
+              offsetLeft,
+            } = range.startContainer.parentElement;
+            key = `${offsetTop + range.startOffset}x${
+              offsetLeft + range.endOffset
+            }`;
           }
           warnings.push(
-              <Warning
-                  key={key}
-                  parentBounds={emailBounds}
-                  message={message}
-                  rangeToHighlight={range}
-              />
+            <Warning
+              key={key}
+              parentBounds={emailBounds}
+              message={message}
+              rangeToHighlight={range}
+            />
           );
         }
       }
@@ -102,9 +116,9 @@ const JustNotSorry = ({ phrases, onEvents }) => {
   if (warnings.length > 0) {
     const email = currentEmail.current;
     const component = (
-        <div data-email-id={email.id} ref={warningContainer}>
-          {warnings}
-        </div>
+      <div data-email-id={email.id} ref={warningContainer}>
+        {warnings}
+      </div>
     );
     return ReactDOM.createPortal(component, email.offsetParent);
   }
