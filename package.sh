41d356ac23f31e587c2df3ec4ad65f20074e01d2
jleo3 <joseph.leo3@gmail.com>
2026-07-22 09:54:41 -0400

    test: preserve release packaging contract

--- package.sh
+++ package.sh
@@ -3,7 +3,9 @@ VERSION_NAME=$1
 VERSION=$(echo "$VERSION_NAME" | sed 's/-beta//')
 sed -i.old "s/\"version\": \".*\"/\"version\": \"$VERSION_NAME\"/" package.json
 sed -i.old -e "s/\"version_name\": \".*\"/\"version_name\": \"$VERSION_NAME\"/" -e "s/\"version\": \".*\"/\"version\": \"$VERSION\"/" manifest.json
-npm run build
+if [ "${SKIP_BUILD:-false}" != "true" ]; then
+  npm run build
+fi
 mkdir -p dist
 cd build
 zip -r "../dist/just-not-sorry-chrome.zip" . *
